41c92be6ee69a51036fc0d9e7a77919344fe330b
Alex DuBois <adubois@alumni.uci.edu>
2025-07-09 23:52:08 -0600

    created main game loop, added venv to gitignore

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    playing = True
+    while playing == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
