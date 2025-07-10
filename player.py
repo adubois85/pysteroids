44132dea944787aefe084eed6a001338910df7d2
Alex DuBois <adubois@alumni.uci.edu>
2025-07-10 16:33:59 -0600

    added forward/back movement, starting pos faces up

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 180
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,6 +18,10 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
     
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+            
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
@@ -28,3 +32,7 @@ class Player(CircleShape):
             self.rotate((-1 * dt))
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move((-1 * dt))
